5b7f255a3b2edb2ce297dd638076cc43e079ac51
Hedgehog44 <amelia.laufejes@gmail.com>
2025-06-22 10:10:05 -0700

    Learning from ChatGPT (still)

--- popup.js
+++ popup.js
@@ -12,14 +12,6 @@ const Steve = [
 "Steve10.png"
 ];
 
-var fs = require('fs');
-var files = fs.readdirSync('.');
-
-function get_random_image() {
-  const random_index = Math.floor(Math.random() * image_array.length);
-  const selected_image = image_array[random_index];
-  document.getElementById('image_shower').src = `./images/${selected_image}`;
-}
 const SteveImage = document.getElementById("Steve-image");
 const changeSteveBtn = document.getElementById("change-Steve");
 
